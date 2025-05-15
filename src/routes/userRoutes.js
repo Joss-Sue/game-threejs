810db68cd62c43abc2ed6e339f08c0d275849dff
Joss-Sue <166410015+Joss-Sue@users.noreply.github.com>
2025-05-15 12:01:03 -0600

    se protegio de mejor forma la ruta que devuelve datos del usuario

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,19 +1,14 @@
 import express from 'express';
+import { requiereLogin } from '../middlewares/requiereLogin.js';
 import UserController from '../db/controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
-router.get('/session', (req, res) => {
-    if (!req.session.usuario) {
-      return res.status(401).json({ message: 'No autenticado' });
-    }
-  
-    // Puedes devolver solo parte del usuario si quieres
-    const { nombre, email, _id } = req.session.usuario;
-    res.json({ nombre, email, _id });
-  });
-  
+router.get('/session', requireLogin, (req, res) => { // protegida
+  const { nombre, email, _id } = req.session.usuario;
+  res.json({ nombre, email, _id });
+});
 
 export default router;
