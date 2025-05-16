0dc4167a2d238b5fdc8d3284881f6fbbb7353615
Joss_Sue <josue_croow@hotmail.com>
2025-05-16 07:21:23 -0600

    habia un error de sintaxis en userroutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { requiereLogin } from '../middlewares/requiereLogin.js';
+import requiereLogin from '../middlewares/requiereLogin.js';
 import UserController from '../db/controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
-router.get('/session', requireLogin, (req, res) => { // protegida
+router.get('/session', requiereLogin, (req, res) => { // protegida
   const { nombre, email, _id } = req.session.usuario;
   res.json({ nombre, email, _id });
 });
